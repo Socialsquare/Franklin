39845a87e03aa8227f8a2a3d8a0aed3fb29d98e8
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2014-01-31 02:40:31 +0100

    Add sensible description to INSTALLED_APPS

--- global_change_lab/settings.py
+++ global_change_lab/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # our own stuff
     'global_change_lab',
     'skills',
+
+    # django packages
     'south',
     'storages',
 )
