90f83671f96edb15d0a2e45bc2992e6b50e9e67a
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2014-02-06 10:49:43 +0100

    Add django-debug-toolbar

--- global_change_lab/settings.py
+++ global_change_lab/settings.py
@@ -130,3 +130,33 @@ LOGGING = {
         },
     }
 }
+
+
+# debug_toolbar settings
+if DEBUG:
+    INTERNAL_IPS = ('127.0.0.1',)
+    MIDDLEWARE_CLASSES += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+
+    INSTALLED_APPS += (
+        'debug_toolbar',
+    )
+
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        # 'debug_toolbar.panels.settings_vars.SettingsVarsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        #'debug_toolbar.panels.profiling.ProfilingPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        # 'debug_toolbar.panels.logger.LoggingPanel',
+    )
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+    }
