1b8d616ae1cacbfc25de3d2f33408e4b87f68357
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2014-02-07 02:13:34 +0100

    Change `render_to_response` to `render` in skills/views.py

--- skills/views.py
+++ skills/views.py
@@ -1,20 +1,20 @@
-from django.http import HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 
 from skills.models import Skill, TrainingBit
 
 def skills_overview(request):
-    return render_to_response('skills/skills_overview.html', {
+    return render(request, 'skills/skills_overview.html', {
         'skills': Skill.objects.all,
     })
 
 def trainingbits_overview(request):
-    return render_to_response('skills/trainingbits_overview.html', {
+    return render(request, 'skills/trainingbits_overview.html', {
         'trainingbits': TrainingBit.objects.all,
     })
 
 def skill(request, skill_id):
     skill = get_object_or_404(Skill, pk=skill_id)
-    return render_to_response('skills/skill.html', {
+    return render(request, 'skills/skill.html', {
         'skill': skill,
     })
