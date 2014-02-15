553fc8ba45868fb48f8c37cf7b7140d6693b514f
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2014-02-15 14:29:04 +0100

    Collect all skill URLs in one place in `urls.py`

--- skills/urls.py
+++ skills/urls.py
@@ -4,8 +4,6 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'skills.views.skills_overview', name="skills_overview"),
-    url(r'^(\d+)$', 'skills.views.skill_view', name='skill_view'),
     url(r'^trainingbits$', 'skills.views.trainingbits_overview', name="trainingbits_overview"),
 
     url(r'^trainingbit/new$', 'skills.views.trainingbit_edit', name='trainingbit_new'),
@@ -15,7 +13,8 @@ urlpatterns = patterns('',
     url(r'^trainingbit/(\d+)/delete$', 'skills.views.trainingbit_delete', name="trainingbit_delete"),
     url(r'^trainingbit/(\d+)/recommend$', 'skills.views.trainingbit_recommend', name="trainingbit_recommend"),
 
-    # TODO: These are stubs, and must be implemented
+    url(r'^$', 'skills.views.skills_overview', name="skills_overview"),
+    url(r'^(\d+)$', 'skills.views.skill_view', name='skill_view'),
     url(r'^skill/new$', 'skills.views.skill_edit', name='skill_new'),
     url(r'^skill/(\d+)/edit$', 'skills.views.skill_edit', name="skill_edit"),
     # url(r'^admin/', include(admin.site.urls)),
