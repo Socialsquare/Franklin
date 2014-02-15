9f8be2694af8f8167ce0e7b8d1a8e0c2a5dabc65
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2014-02-15 23:18:17 +0100

    [Fix] Use `get_object_or_404` on profile page + fix spelling err

--- global_change_lab/views.py
+++ global_change_lab/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 import django.contrib.messages as messages
 from django.contrib.auth.models import Group
@@ -37,7 +37,7 @@ def profile(request, user_id=None):
     if user_id is None:
         user = request.user
     else:
-        user = User.objects.get(id__exact=user_id)
+        user = get_object_or_404(User, pk=user_id)
     return render(request, 'profile.html', {
         'some_user': user,
         'user_fields': User._meta.get_all_field_names(),
@@ -77,7 +77,7 @@ def user_upgrade_to_trainer(request, user_id):
         g = Group.objects.get(name='Trainers')
         g.user_set.add(user)
 
-        messages.success(request, 'Successfully upgrade %s to be a trainer.' % user.username)
+        messages.success(request, 'Successfully upgraded %s to be a trainer.' % user.username)
     else:
         messages.error(request, 'You do not have permissions to upgrade this user.')
 
