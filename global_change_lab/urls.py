aa16dfeb1812091c6950563783b5a599c8979705
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2014-02-18 14:51:21 +0100

    [Update] Structure urls a bit different
    Don't go "[BASE_URL]/skills/trainingbits". Change it to the better
    "[BASE_URL]/trainingbits". Also some code restructuring (move stuff
    around).

--- global_change_lab/urls.py
+++ global_change_lab/urls.py
@@ -8,16 +8,18 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'global_change_lab.views.front_page', name='front_page'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^skills/', include('skills.urls', namespace='skills')),
+    url(r'^', include('skills.urls', namespace='skills')),
 
     url(r'^shares$', 'global_change_lab.views.shares', name='shares'),
 
     # django-allauth
     # url(r'^user/', include('allauth.urls', namespace='allauth', app_name='user')),
-    url(r'^user/list', 'global_change_lab.views.user_list', name='user_list'),
-    url(r'^user/dashboard', 'global_change_lab.views.trainer_dashboard', name='trainer_dashboard'),
     url(r'^user/', include('allauth.urls')),
+
+    # global-change-lab
+    url(r'^user/list', 'global_change_lab.views.user_list', name='user_list'),
     url(r'^user/profile/(\d+)?', 'global_change_lab.views.profile', name='profile'),
+    url(r'^user/dashboard', 'global_change_lab.views.trainer_dashboard', name='trainer_dashboard'),
     url(r'^user/delete/(\d+)', 'global_change_lab.views.user_delete', name='user_delete'),
     url(r'^user/make_trainer/(\d+)', 'global_change_lab.views.user_upgrade_to_trainer', name='user_upgrade_to_trainer'),
 
