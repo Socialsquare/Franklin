d7a5eb6452931ddb638146046bcc6c5ceab2aeee
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2014-02-23 17:14:25 +0100

    [Add] Auto-save on training-bit
    Plus add <h1>, <h2>, <ul>, <ol> to the editor

--- Gruntfile.js
+++ Gruntfile.js
@@ -124,6 +124,9 @@ module.exports = function(grunt) {
             'bower_components/jqueryui/ui/jquery.ui.widget.js',
             'bower_components/jqueryui/ui/jquery.ui.mouse.js',
             'bower_components/jqueryui/ui/jquery.ui.sortable.js',
+            'bower_components/jquery-throttle-debounce/jquery.ba-throttle-debounce.js',
+            'bower_components/jquery-form/jquery.form.js',
+            'bower_components/offline/offline.min.js',
           ]
         }
       }
