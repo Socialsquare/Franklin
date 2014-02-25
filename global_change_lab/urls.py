d08d34577e473bff930b5f473cf37fa4dd60d306
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2014-02-25 01:06:26 +0100

    [Cleanup] Structure global_change_lab/urls.py
    + Rename 'user/dashboard' to 'trainer/dashboard' (makes more sense)
    + Remove "shares" URL for now (affects front_page.html)
    + Remove unused comments

--- global_change_lab/urls.py
+++ global_change_lab/urls.py
@@ -5,35 +5,37 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'global_change_lab.views.front_page', name='front_page'),
-    url(r'^first/?$', 'global_change_lab.views.new_user', name='new_user'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^', include('skills.urls', namespace='skills')),
 
-    url(r'^shares$', 'global_change_lab.views.shares', name='shares'),
+    # global-change-lab
+    #   front-page and welcome
+    url(r'^$', 'global_change_lab.views.front_page', name='front_page'),
+    url(r'^welcome/?$', 'global_change_lab.views.new_user', name='new_user'),
+    #   users
+    url(r'^user/list/?$', 'global_change_lab.views.user_list', name='user_list'),
+    url(r'^user/me/?$', 'global_change_lab.views.user_progress', name='user_progress'),
+    url(r'^user/profile/(\d+)?$', 'global_change_lab.views.profile', name='profile'),
+    url(r'^user/profile/upload/?$', 'global_change_lab.views.upload_profile_picture', name='upload_profile_picture'),
+    url(r'^user/delete/(\d+)/?$', 'global_change_lab.views.user_delete', name='user_delete'),
+    url(r'^user/make_trainer/(\d+)/?$', 'global_change_lab.views.user_upgrade_to_trainer', name='user_upgrade_to_trainer'),
+    #   trainers
+    url(r'^trainer/dashboard/?$', 'global_change_lab.views.trainer_dashboard', name='trainer_dashboard'),
+    url(r'^trainer/dashboard/statistics/?$', 'global_change_lab.views.statistics', name='statistics'),
+    #   admins
+    url(r'^admin/dashboard/?$', 'global_change_lab.views.admin_dashboard', name='admin_dashboard'),
+    url(r'^admin/users.csv$', 'global_change_lab.views.admin_users_csv', name='admin_users_csv'),
+    url(r'^admin/statistics.csv$', 'global_change_lab.views.admin_statistics_csv', name='admin_statistics_csv'),
+
+    # skills
+    url(r'^', include('skills.urls', namespace='skills')),
 
     # django-allauth
     # url(r'^user/', include('allauth.urls', namespace='allauth', app_name='user')),
     url(r'^user/', include('allauth.urls')),
 
-    # global-change-lab
-    url(r'^user/list/?$', 'global_change_lab.views.user_list', name='user_list'),
-    url(r'^user/me/?$', 'global_change_lab.views.user_progress', name='user_progress'),
-    url(r'^user/profile/(\d+)?$', 'global_change_lab.views.profile', name='profile'),
-    url(r'^user/profile/upload/?$', 'global_change_lab.views.upload_profile_picture', name='upload_profile_picture'),
-    url(r'^user/dashboard/?$', 'global_change_lab.views.trainer_dashboard', name='trainer_dashboard'),
-    url(r'^user/dashboard/statistics/?$', 'global_change_lab.views.statistics', name='statistics'),
-    url(r'^admin/dashboard/?$', 'global_change_lab.views.admin_dashboard', name='admin_dashboard'),
-    url(r'^admin/users.csv$', 'global_change_lab.views.admin_users_csv', name='admin_users_csv'),
-    url(r'^admin/statistics.csv$', 'global_change_lab.views.admin_statistics_csv', name='admin_statistics_csv'),
-    url(r'^user/delete/(\d+)/?$', 'global_change_lab.views.user_delete', name='user_delete'),
-    url(r'^user/make_trainer/(\d+)/?$', 'global_change_lab.views.user_upgrade_to_trainer', name='user_upgrade_to_trainer'),
-
     # django-comments-xtd
     url(r'^comments/', include('django_comments_xtd.urls')),
 
-    # url(r'^/', include(admin.site.urls)),
+    # django.contrib.admin
     url(r'^admin/', include(admin.site.urls)),
 )
 
