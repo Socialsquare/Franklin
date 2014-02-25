6659c00b8cb5157ad57fd44d1d55efa6f9ea3974
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2014-02-25 23:30:44 +0100

    [Add] Ability to edit users in more detail in the django admin

--- global_change_lab/admin.py
+++ global_change_lab/admin.py
@@ -10,18 +10,30 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
 
+
 class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
 
+    exclude_fields = ['id', 'date_joined']
+    print('user fields', exclude_fields)
+
+    _all_fields = [f.name for f in User._meta.fields]
+    _fields = list(set(_all_fields) - set(exclude_fields))
+
+    # List comprehensions and lambdas in the class definition cannot use class
+    # variables from the class definition. This is due to their scope rules.
+    # See: http://stackoverflow.com/questions/13905741/accessing-class-variables-from-a-list-comprehension-in-the-class-definition
+    # This means that the following will _not_ work:
+    # _all_fields = [f.name for f in User._meta.fields if f.name not in exclude_fields]
+
     fieldsets = (
-        (None, { 'fields': ('email',) }),
+        (None, { 'fields':
+            _fields
+        }),
     )
-    # fieldsets = UserAdmin.fieldsets + (
-    #     (None, {'fields': (,)}),
-    # )
 
     # Columns shown in admin
-    list_display = ('username', 'email', )
+    list_display = ('username', 'email', 'is_superuser')
     # Filters shown in admin
     list_filter = ('is_superuser', )
 
