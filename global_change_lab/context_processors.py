ee5b46a81f81820316896455a1bf473711f1cf30
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2014-03-12 03:36:48 +0100

    [Update] fat_footer: Add recent likes to right column

--- global_change_lab/context_processors.py
+++ global_change_lab/context_processors.py
@@ -1,12 +1,26 @@
-from skills.models import Project
-from skills.models import Comment
+from skills.models import Project, Comment, Like
 
 def fatfooter_data(request):
+    recent_activity_count = 7
+
+    recent_comments = Comment.objects.all().order_by('-created_at')[:recent_activity_count]
+    recent_likes    = Like.objects.all().order_by('-created_at')[:recent_activity_count]
+
+    recent_activity = list(recent_comments) + list(recent_likes)
+    recent_activity.sort(key=lambda a: a.created_at, reverse=True)
+
+    l = []
+    for activity in recent_activity:
+        if type(activity) == Like:
+            l.append((activity.author, 'liked', activity.content_object))
+        elif type(activity) == Comment:
+            l.append((activity.author, 'commented on', activity.project))
+
     return {
         'recent_projects': Project.objects.all()                 \
                             .select_related('author__username') \
                             .select_related('author__image')    \
                             .order_by('-created_at')[:3],
-        'recent_comments': Comment.objects.all().order_by('-created_at')[:5]
+        'recent_activity': l[:recent_activity_count]
     }
 
