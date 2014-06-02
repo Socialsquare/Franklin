77232ac728dc48eafebe98ec2b21aa530bb1bc1c
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-06-02 15:01:53 +0200

    Adding the admin model for projects.

--- skills/admin.py
+++ skills/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from skills.models import Skill, TrainingBit
+from skills.models import Skill, TrainingBit, Project
 
 # Register your models here.
 admin.site.register(TrainingBit)
 admin.site.register(Skill)
+admin.site.register(Project)
