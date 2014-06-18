f014cd92834ebe9648f5b5f7a25e01f24c2ccf0e
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-06-18 13:50:13 +0200

    Adding debugging_production setting, with Debug=true. Don't push this to production.

---
+++ global_change_lab/settings_debugging_production.py
@@ -0,0 +1,14 @@
+"""
+Django settings for global_change_lab project in production.
+
+!! THIS FILE IS CONFIDENTIAL !!
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+from .settings_production import *
+
+DEBUG = True
