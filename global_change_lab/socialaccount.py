ca8eab161cc87b9c97f1c617f4f91089883a09f8
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-06-23 14:09:13 +0200

    Adding Google+ support to Podio issue 127

--- global_change_lab/socialaccount.py
+++ global_change_lab/socialaccount.py
@@ -13,6 +13,7 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
 		name = data.get('name')
 		username = data.get('username')
 		user = sociallogin.account.user
+		print(sociallogin.account.provider)
 		# TODO: Consider using the super class for the stuff that is not unique to facebook!
 		if sociallogin.account.provider == 'facebook':
 			user_email(user, valid_email_or_none(email) or '')
@@ -33,8 +34,21 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
 			name_parts = (name or '').partition(' ')
 			user_field(user, 'first_name', first_name or name_parts[0])
 			user_field(user, 'last_name', last_name or name_parts[2])
-			print("sociallogin.account.extra_data:", str(sociallogin.account.extra_data).encode('utf-8'))
 			# Save a good username
+			if username:
+				username_suggestions = [username, first_name, last_name, email, 'user']
+			else:
+				username_suggestions = [first_name, last_name, email, 'user']
+			user_username(user, generate_unique_username(username_suggestions))
+			return user
+		elif sociallogin.account.provider == 'google':
+			#if "screen_name" in sociallogin.account.extra_data.keys():
+			#	username = sociallogin.account.extra_data['screen_name']
+			user_email(user, valid_email_or_none(email) or '')
+			name_parts = (name or '').partition(' ')
+			user_field(user, 'first_name', first_name or name_parts[0])
+			user_field(user, 'last_name', last_name or name_parts[2])
+			print("sociallogin.account.extra_data:", str(sociallogin.account.extra_data).encode('utf-8'))
 			if username:
 				username_suggestions = [username, first_name, last_name, email, 'user']
 			else:
@@ -112,5 +126,39 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
 			# Saving the desciption
 			if "description" in sociallogin.account.extra_data.keys():
 				user.description = sociallogin.account.extra_data['description']
+				user.description += "\nFollow me on Twitter: https://twitter.com/%s" % sociallogin.account.extra_data['screen_name']
+			user.save()
+		elif sociallogin.account.provider == 'google':
+			# Generate the Facebook image URL
+			if "picture" in sociallogin.account.extra_data.keys():
+				profile_picture_url = sociallogin.account.extra_data['picture']
+				# Download the Facebook profile image.
+				profile_image_r = requests.get(profile_picture_url)
+				profile_image_temp = NamedTemporaryFile(delete=True)
+				profile_image_temp.write(profile_image_r.content)
+				profile_image_temp.flush()
+				profile_image_file = File(profile_image_temp)
+				# We have to use save=False, otherwise saving the image field saves the user model.
+				user.image.save("%s.jpg" % sociallogin.account.uid, File(profile_image_temp))
+
+			# Gender
+			if "gender" in sociallogin.account.extra_data.keys():
+				gender = sociallogin.account.extra_data['gender']
+				# Need to hang on to the userinfo object to save it.
+				userinfo = user.userinfo
+				if gender == "male" or gender == "female":
+					userinfo.sex = gender
+					userinfo.save()
+
+			descriptions = []
+			# Creating a bio
+			if "name" in sociallogin.account.extra_data.keys():
+				name = sociallogin.account.extra_data['name']
+				descriptions.append( "My name is %s." % name )
+			if "link" in sociallogin.account.extra_data.keys():
+				link = sociallogin.account.extra_data['link']
+				descriptions.append( "Visit my Google+ profile: %s" % link )
+			user.description = "\n".join(descriptions)
+			# Save the model!
 			user.save()
 		return user
