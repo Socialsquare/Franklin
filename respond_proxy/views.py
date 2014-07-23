74dd8a43af051f5457229267dcb1668aa18d580a
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-07-23 13:05:36 +0200

    Changing respond proxy paths.

--- respond_proxy/views.py
+++ respond_proxy/views.py
@@ -6,12 +6,12 @@ SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 # Create your views here.
 def respond_proxy_js(request):
-	file_path = "%s/static/respond.proxy.js" % SITE_ROOT
+	file_path = "%s/static/respond_proxy/respond.proxy.js" % SITE_ROOT
 	file_data = open(file_path, "rb").read()
 	return HttpResponse(file_data, mimetype="application/javascript")
 
 # Create your views here.
 def respond_proxy_gif(request):
-	file_path = "%s/static/respond.proxy.gif" % SITE_ROOT
+	file_path = "%s/static/respond_proxy/respond.proxy.gif" % SITE_ROOT
 	file_data = open(file_path, "rb").read()
 	return HttpResponse(file_data, mimetype="image/gif")
