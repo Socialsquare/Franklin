f6b24175773b0a8bbae3c99764b65d68215d9448
Niles Christensen <nilesnchristensen@gmail.com>
2014-09-14 11:54:34 +0200

    Adds training bit completion test
    Correctly renames the test that was previously called “test_complete_
    skill” to “test_complete_skill”, and then writes a full test_complete_
    skill test.

--- tests.py
+++ tests.py
@@ -53,19 +53,44 @@ class MySeleniumTests(LiveServerTestCase):
         MySeleniumTests.login(self)
         assert('%s%s' % (self.live_server_url, '/welcome') in self.selenium.current_url)
 
-    def test_complete_skill(self):
-        """A specific skill can be completed"""
+    def test_complete_training_bit(self):
+        """A specific trainingbit can be completed"""
         MySeleniumTests.login(self)
         self.selenium.get('%s%s' % (self.live_server_url, '/trainingbit/punch-an-angry-shark/view'))
         self.selenium.find_element_by_name('name').send_keys(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20)))
         self.selenium.find_element_by_name('content').send_keys(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20)))
         self.selenium.find_element_by_class_name('share-button').click()
 
-        print(self.selenium.current_url)
-
-
         assert('%s%s' % (self.live_server_url, '/share/') in self.selenium.current_url)
 
+    def test_complete_skill(self):
+        """A specific skill can be completed"""
+        MySeleniumTests.login(self)
+        self.selenium.get('%s%s' % (self.live_server_url, '/skills'))
+        self.selenium.find_element_by_name('skill-name').click()
+        # buttons = self.selenium.find_elements_by_name('start-button')
+        # print(self.selenium.current_url)
+        # correct_button = [button for button in buttons if button.is_displayed()]
+        # correct_button[0].click()
+        time.sleep(1)
+        self.selenium.find_element_by_name('start-button').click()
+        # self.selenium.find_element_by_class_name('trainingbit-name').click()
+
+        # self.selenium.get('%s%s' % (self.live_server_url, '/trainingbit/punch-an-angry-shark/view'))
+        self.selenium.get('%s%s' % (self.live_server_url, '/trainingbit/run-away-from-trouble/view'))
+        self.selenium.find_element_by_name('name').send_keys(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20)))
+        self.selenium.find_element_by_name('content').send_keys(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20)))
+        self.selenium.find_element_by_class_name('share-button').click()
+
+        print(self.selenium.current_url)
+        self.selenium.find_element_by_class_name('skill-flag')
+        assert('%s%s' % (self.live_server_url, '/share/') in self.selenium.current_url)
+
+
+        # for pdiff, use http://scipy-lectures.github.io/advanced/image_processing/. create an array of arrays, each of which stores points.
+        # For each point that's different, look for other different points within 10 pixels on each side (maybe change later)
+        # Every point that's within 10 pixels, add to the array, and look for points within 10 pixels there. After that, move on to the next
+        # Point that hasn't already been added, and add it as an entirely new array of points, repeat until all the points are gone
 
 
 
